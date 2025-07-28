2104706887d2033b319aed7c6717b9865a5054f0
Joris Cosentino <joris.cosentino.pro@gmail.com>
2025-07-28 21:44:42 +0200

    make add_background faster

--- audiomentations/core/audio_loading_utils.py
+++ audiomentations/core/audio_loading_utils.py
@@ -4,7 +4,7 @@ import librosa
 import numpy as np
 
 
-def load_sound_file(file_path, sample_rate, mono=True, resample_type="auto"):
+def load_sound_file(file_path, sample_rate, mono=True, resample_type="auto", offset=0, duration=None):
     """
     Load an audio file as a floating point time series. Audio will be automatically
     resampled to the given sample rate.
@@ -17,7 +17,7 @@ def load_sound_file(file_path, sample_rate, mono=True, resample_type="auto"):
     """
     file_path = str(file_path)
     samples, actual_sample_rate = librosa.load(
-        str(file_path), sr=None, mono=mono, dtype=np.float32
+        str(file_path), sr=None, offset=offset, duration=duration, mono=mono, dtype=np.float32
     )
 
     if sample_rate is not None and actual_sample_rate != sample_rate:
